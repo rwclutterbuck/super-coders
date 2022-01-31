fd444a12dda390bdc60e3072308ff589cf72960e
rwclutterbuck <rwclutterbuck@outlook.com>
2022-01-31 17:00:24 +0000

    add templates to index.js

--- client/src/js/index.js
+++ client/src/js/index.js
@@ -1,8 +1,68 @@
-const hamburger = document.querySelector('[aria-label="toggle menu"]');
-const menu = document.querySelector('#dropdown-menu');
+// Templates
+require("./templates/navBarTemplate");
+require("./templates/footerTemplate");
+const blogCard = require("./templates/cardTemplate");
 
-hamburger.addEventListener('click', (e) => {
+const hamburger = document.querySelector('[aria-label="toggle menu"]');
+const menu = document.querySelector("#dropdown-menu");
+const cardContainer = document.querySelector("#card-container");
+
+hamburger.addEventListener("click", (e) => {
   e.preventDefault();
-  console.log('click');
-  menu.classList.toggle('hide-menu');
-})
+  console.log("click");
+  menu.classList.toggle("hide-menu");
+});
+
+cardContainer.innerHTML += blogCard(
+  "I Built A Successful Blog In One Year",
+  `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Molestie
+  parturient et sem ipsum volutpat vel. Natoque sem et aliquam mauris
+  egestas quam volutpat viverra. In pretium nec senectus erat. Et
+  malesuada lobortis.`,
+  "https://images.unsplash.com/photo-1550439062-609e1531270e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"
+);
+
+cardContainer.innerHTML += blogCard(
+  "Testing",
+  `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Molestie
+  parturient et sem ipsum volutpat vel. Natoque sem et aliquam mauris
+  egestas quam volutpat viverra. In pretium nec senectus erat. Et
+  malesuada lobortis.`,
+  "../images/splash-screen.jpg"
+);
+
+cardContainer.innerHTML += blogCard(
+  "I Built A Successful Blog In One Year",
+  `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Molestie
+  parturient et sem ipsum volutpat vel. Natoque sem et aliquam mauris
+  egestas quam volutpat viverra. In pretium nec senectus erat. Et
+  malesuada lobortis.`,
+  "https://images.unsplash.com/photo-1550439062-609e1531270e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"
+);
+
+cardContainer.innerHTML += blogCard(
+  "Testing",
+  `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Molestie
+  parturient et sem ipsum volutpat vel. Natoque sem et aliquam mauris
+  egestas quam volutpat viverra. In pretium nec senectus erat. Et
+  malesuada lobortis.`,
+  "../images/splash-screen.jpg"
+);
+
+cardContainer.innerHTML += blogCard(
+  "I Built A Successful Blog In One Year",
+  `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Molestie
+  parturient et sem ipsum volutpat vel. Natoque sem et aliquam mauris
+  egestas quam volutpat viverra. In pretium nec senectus erat. Et
+  malesuada lobortis.`,
+  "https://images.unsplash.com/photo-1550439062-609e1531270e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"
+);
+
+cardContainer.innerHTML += blogCard(
+  "Testing",
+  `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Molestie
+  parturient et sem ipsum volutpat vel. Natoque sem et aliquam mauris
+  egestas quam volutpat viverra. In pretium nec senectus erat. Et
+  malesuada lobortis.`,
+  "../images/splash-screen.jpg"
+);
