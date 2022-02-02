5054d1561e0458336192216d82a0e5d7030f9831
Noah Platton <noahplatton@gmail.com>
2022-02-02 12:56:09 +0000

    Add gif to post request works

--- src/js/index.js
+++ src/js/index.js
@@ -21,11 +21,11 @@ form && form.addEventListener("submit", handlers.postBlog);
 // Identify the page in the browser
 const location = window.location.pathname;
 
-let blogId;
-if (location === "/blog.html") {
-  let blog = window.localStorage.getItem("blogId");
-  blogId = window.localStorage.getItem(`blogId-${blog}`);
-}
+// let blogId;
+// if (location === "/blog.html") {
+//   let blog = window.localStorage.getItem("blogId");
+//   blogId = window.localStorage.getItem(`blogId-${blog}`);
+// }
 
 // Call fcts depending on the browser page
 switch (location) {
@@ -44,15 +44,15 @@ switch (location) {
 
 blogPreview && (blogPreview.innerHTML = blogCard());
 
-const numCards = document.querySelector("#card-container").childNodes.length;
+// const numCards = document.querySelector("#card-container").childNodes.length;
 
-const cards = document.getElementsByClassName("blog-card");
-// console.log(cards);
+// const cards = document.getElementsByClassName("blog-card");
+// // console.log(cards);
 
-cards.addEventListener("click", (e) => {
-  e.preventDefault();
-  console.log(e);
-});
+// cards.addEventListener("click", (e) => {
+//   e.preventDefault();
+//   console.log(e);
+// });
 
 // for (let i = 1; i <= numCards; i++) {
 //   let cardId = `#card-link-${i}`;
